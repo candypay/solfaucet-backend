a99e1e0dfa4d9f224e5aa1dfe178665dd46e536d
Vampo7152 <umangboss786@gmail.com>
2023-07-09 07:54:15 +0000

    fix: another method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
-import cors from "cors";
+// import cors from "cors";
 import "dotenv/config";
 
 import { router } from "@/router";
 
 const app: Application = express();
+const allowedOrigins = ['https://solfaucet.fun/'];
 
 app.get("/", (_req: Request, res: Response) => {
   return res.status(200).json({
@@ -13,10 +14,30 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://solfaucet.fun/',
-  methods: ['GET', 'POST'],
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (!origin) { 
+    return res.status(403).json({
+      "error": "Access denied"
+     })
+  }
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    // Requests from other origins will receive a 403 Forbidden response
+    return res.status(403).json({
+      error: 'Access denied'
+    });
+  }
+  
+  // You can modify other CORS headers and options as needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+
+  next();
+});
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
 
